133d49047b17b72065c6d5832920daad1642bf9a
greg-sk <grzegorz.luczywo@securitykiss.com>
2015-05-26 16:41:58 +0200

    checksums

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 ls -t1 --hide=latest.txt --hide=README.md --hide=update.sh > latest.txt
+
+cat latest.txt | xargs sha256sum > latest-sha256.txt
+cat latest.txt | xargs sha1sum > latest-sha1.txt
+cat latest.txt | xargs md5sum > latest-md5.txt
+
